eecb9253827c81c5a4d5af7bde9ed2e6dd8d6f5e
jomrz <joemoralez@outlook.com>
2017-05-26 16:24:04 -0700

    Starting creating swarming targets
    Direction is defined at instantiation. Must make movement dynamic. Also, at acceleration to targets.

--- target.js
+++ target.js
@@ -1,12 +1,22 @@
 //Target Class
 
+
 var Target = function(pos, rad, points) {
-    this.acceleration = createVector(0, 0.01);
-    this.velocity = createVector(random(-1, 1), random(0, 1));
     this.position = createVector(pos.x, pos.y)
-    this.position = createVector(pos.x, pos.y)
-    this.radius = rad
+	//this.velocity = createVector(random(-1, 1), random(0, 1));
+	this.velocity = 0.05
+	
+	this.dy = ship.position.y - this.position.y
+	this.dx = ship.position.x - this.position.x
+	this.angle = atan2(this.dy,this.dx)
+	this.vx = (this.velocity * cos(this.angle))
+	this.vy = (this.velocity * sin(this.angle))
+	
+    this.directionVector = createVector(this.vx, this.vy);
+   
+	this.radius = rad
     this.points = points
+	
 
     this.run = function() {
         this.update()
@@ -32,8 +42,7 @@ var Target = function(pos, rad, points) {
     }
 
     this.update = function() {
-        this.velocity.add(this.acceleration)
-        this.position.add(this.velocity)
+        //this.velocity.add(this.acceleration)
+        this.position.add(this.directionVector)
     }
-
 }
