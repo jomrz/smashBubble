84034669edc80e959e4578a9682bd41c954c7541
jomrz <joemoralez@outlook.com>
2017-06-01 10:30:04 -0700

    Targets follow ship
    Targets dynamically follow the ship.
    Broke variables into functions. angle() returns the angle between the target and ship. heading() returns a vector to add to the velocity.

--- target.js
+++ target.js
@@ -1,48 +1,55 @@
-//Target Class
-
-
 var Target = function(pos, rad, points) {
-    this.position = createVector(pos.x, pos.y)
-	//this.velocity = createVector(random(-1, 1), random(0, 1));
-	this.velocity = 0.05
 	
-	this.dy = ship.position.y - this.position.y
-	this.dx = ship.position.x - this.position.x
-	this.angle = atan2(this.dy,this.dx)
-	this.vx = (this.velocity * cos(this.angle))
-	this.vy = (this.velocity * sin(this.angle))
+	let self = this
+	this.position = createVector(pos.x, pos.y)
+	this.velocity = 1
 	
-    this.directionVector = createVector(this.vx, this.vy);
-   
+
+
+	function angle(){
+		let dy = ship.position.y - self.position.y
+		let dx = ship.position.x - self.position.x
+		return atan2(dy, dx)
+	}
+
+
+	function heading() {
+		let vx = self.velocity * cos(angle())
+		let vy = self.velocity * sin(angle())
+		console.log(vx + " " + vy)
+		return createVector(vx, vy)
+	}
+
+	//this.directionVector = () => createVector(this.vx, this.vy);
+
 	this.radius = rad
-    this.points = points
+	this.points = points
 	
 
-    this.run = function() {
-        this.update()
-        this.display()
-    }
+	this.run = function() {
+		this.update()
+		this.display()
+	}
 
-    this.alive = true
+	this.alive = true
 
-    this.display = function() {
-        push()
-        
-        translate(this.position.x, this.position.y)
-        fill(255, 100)
-        stroke(255, 180)
-        ellipse(0, 0, this.radius * 2, this.radius * 2)
+	this.display = function() {
+		push()
+		
+		translate(this.position.x, this.position.y)
+		fill(255, 100)
+		stroke(255, 180)
+		ellipse(0, 0, this.radius * 2, this.radius * 2)
 
-        pop()
-    }
+		pop()
+	}
 
-    this.isHit = function(index) {
-        this.alive = false
+	this.isHit = function(index) {
+		this.alive = false
 
-    }
+	}
 
-    this.update = function() {
-        //this.velocity.add(this.acceleration)
-        this.position.add(this.directionVector)
-    }
+	this.update = function() {
+		this.position.add(heading()) 
+	}
 }
